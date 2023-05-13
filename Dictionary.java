4f602cb5dcd5a7a492dc7f79b9c8944010c54b09
0marA <theengineermachine@gmail.com>
2023-05-13 18:08:22 -0500

    Dictionary filtering for words

--- Dictionary.java
+++ Dictionary.java
@@ -2,36 +2,35 @@ import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
+
 public class Dictionary {
-    ArrayList<String> dictionary = new ArrayList<>();
+    ArrayList<String> dictionary;
 
 
     public Dictionary() {
-        // getDictionary();
-        dictionary.add("monkey");
-        dictionary.add("rabbit");
-        dictionary.add("laptop");
+        dictionary = new ArrayList<>();
+         readDictionary();
+        // dictionary.add("ulmic");
+        // dictionary.add("assume");
+        // dictionary.add("sum");
+        // dictionary.add("museum");
+        // dictionary.add("laptop");
     }
 
     private void readDictionary() {
         try {
-            File dictionaryFile = new File("dictionary.txt");
-            Scanner fileReader = new Scanner(dictionaryFile);
-            while (fileReader.hasNextLine()) {
+            Scanner fileReader = new Scanner(new File("dictionary.txt"));
+            while (fileReader.hasNextLine()) 
                 dictionary.add(fileReader.nextLine());
-            }
+    
             fileReader.close();
             
           } catch (FileNotFoundException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
+            System.out.println("An error occurred trying to parse the dictionary.");
           }
     }
 
     protected ArrayList<String> getDictionaryArrayList() {
         return dictionary;
     } 
-
-    
-    
 }
--- Permutations.java
+++ Permutations.java
@@ -1,15 +1,17 @@
 import java.util.ArrayList;
+
 public class Permutations extends Dictionary {
 
     private ArrayList<Character> permutatableLetters;
+    private ArrayList<String> dictionary;
     private ArrayList<String> possibleWords;
-    
+    private String inputtedLetters;
+
     public Permutations() {
         super();
+        dictionary = getDictionaryArrayList();
         permutatableLetters = new ArrayList<>();
         possibleWords = new ArrayList<>();
-
-
     }
 
     public ArrayList<String> getPermutations() {
@@ -20,26 +22,49 @@ public class Permutations extends Dictionary {
     }
 
     public void setPossibleLetters(String possibleLettersString) {
+        inputtedLetters = possibleLettersString;
         for (int i = 0; i < possibleLettersString.length(); i++) {
             permutatableLetters.add(possibleLettersString.charAt(i));
         }
     }
 
     public void filterDictionary() {
-        for (int dictionaryLine = 0; dictionaryLine < getDictionaryArrayList().size(); dictionaryLine++) {
-            for (int letterAtLineOfDictionary = 0; letterAtLineOfDictionary < getDictionaryArrayList().get(dictionaryLine).length(); letterAtLineOfDictionary++) {
-                for (int possibleLetter = 0; possibleLetter < permutatableLetters.size(); possibleLetter++) {
-                    if (getDictionaryArrayList().get(dictionaryLine).charAt(letterAtLineOfDictionary) == permutatableLetters.get(possibleLetter) && 
-                        !possibleWords.contains(getDictionaryArrayList().get(dictionaryLine))) {
-                        possibleWords.add(getDictionaryArrayList().get(dictionaryLine));
-                    }
+        boolean skip = false;
 
+        for (int dicLine = 0; dicLine < dictionary.size(); dicLine++) {
+            ArrayList<Character> temporaryPermutableLetters = new ArrayList<>(permutatableLetters);
+
+            for (int dicLineLetter = 0; dicLineLetter < dictionary.get(dicLine)
+                    .length(); dicLineLetter++) {
+
+                if (dictionary.get(dicLine).equals(inputtedLetters))
+                    break;
+
+
+                if (!permutatableLetters.contains(dictionary.get(dicLine).charAt(dicLineLetter))
+                        || dictionary.get(dicLine).length() > inputtedLetters.length()
+                        || dictionary.get(dicLine).length() < 3) {
+                    skip = true;
+                    break;
+                }
+
+                try {
+                    temporaryPermutableLetters.remove(temporaryPermutableLetters
+                            .indexOf(dictionary.get(dicLine).charAt(dicLineLetter)));
+                } catch (Exception e) {
+                    skip = true;
+                    break;
                 }
 
             }
+            if (!skip)
+                possibleWords.add(dictionary.get(dicLine));
+            skip = false;
         }
 
         System.out.println(possibleWords);
     }
-    
+
+
+
 }
